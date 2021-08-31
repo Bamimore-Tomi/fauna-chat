5699621accbd8b64da6bf9dc5fe307a6dc02fbb3
Bamimore-Tomi <tomibami2020@gmail.com>
2021-08-31 23:50:31 +0100

    working on tests for register function

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, random, string
 
 from main import app
 
@@ -11,5 +11,16 @@ class TestCases(unittest.TestCase):
 
     def test_register(self):
         with app.test_client(self) as client:
-            post_response = client.post("/register", data={"email": ""})
+            # Generate dummy username
+            username = "".join([random.choice(string.ascii_letters) for i in range(7)])
+            post_response_1 = client.post(
+                "/register",
+                data={
+                    "email": username + "@gmail.com",
+                    "username": f"test-{username}",
+                    "password": f"password-{username}",
+                },
+            )
+            # test is successful registration return a redirect to the login page
+            self.assertEqual(post_response_1.status_code, 302)
         post_detials = tester.post()
